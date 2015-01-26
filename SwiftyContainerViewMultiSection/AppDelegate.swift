b259dc55d0d46d589a645265789a01cdff1c1732
dennis-walsh <dennisw@mirthcorp.com>
2015-01-25 21:46:26 -0800

    remove crashalytics framework

--- SwiftyContainerViewMultiSection/AppDelegate.swift
+++ SwiftyContainerViewMultiSection/AppDelegate.swift
@@ -7,8 +7,8 @@
 //
 
 import UIKit
-import Fabric
-import Crashlytics
+//import Fabric
+//import Crashlytics
 
 
 @UIApplicationMain
@@ -19,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
-        Fabric.with([Crashlytics()])
+        //Fabric.with([Crashlytics()])
         return true
     }
 
